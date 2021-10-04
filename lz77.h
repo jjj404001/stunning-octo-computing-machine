29ba5a0e74678ecf40d5fdf1c220a0e7d3512106
Jaejun Jang <jjj404001@gmail.com>
2021-10-03 20:49:38 -0700

    Use src buffer as view buffer temporary.

--- lz77.h
+++ lz77.h
@@ -3,6 +3,7 @@ Implementation of lz77 encoding algorith.
 */
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 void* EncodeLZ77(void* _src, size_t _size)
 {
@@ -23,19 +24,36 @@ void* EncodeLZ77(void* _src, size_t _size)
     need to revisit LCS...
     */
 
-    const size_t window_size = 0x06;
-    const size_t view_size   = 0x04;
+    uint8_t* byte_src = (uint8_t*)_src;
 
-    void* window = malloc(window_size);
-    void* view   = malloc(0x04);
+    const size_t window_count = 0x06;
+    const size_t view_count   = 0x04;
 
-    while(_size > window_size)
+    const size_t window_size = window_count * sizeof(uint8_t);
+    const size_t view_size   = view_count   * sizeof(uint8_t);
+
+    uint8_t* window = (uint8_t*)malloc(window_size);
+    uint8_t* view   = (uint8_t*)malloc(view_size);
+
+    uint64_t window_index = 0;
+    uint64_t view_index   = 0;
+
+    // Fill view buffer with inputs.
+
+    while(_size > view_size)
     {
-        uint8_t p; 
+        memcpy(view, &byte_src[view_index], view_size); // use byte_src as view buffer.
+
+        
+
+        
+        _size -= view_size;
     }
 
-    if(_size > 0)
-    {
 
-    }
+
+
+
+    free(window);
+    free(view);
 }
