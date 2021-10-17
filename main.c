5a670cd8aed372cba7908c6312a2b349289de91c
Jaejun Jang <jjj404001@gmail.com>
2021-10-16 22:27:42 -0700

    Add testing.

--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 #include "lz77.h"
 
+int print_test(void* _buff, size_t _type_size, size_t _buff_count)
+{
+    LzNode* node = EncodeLZ77(_buff, _type_size * _buff_count);
+
+    
+    while(node)
+    {
+        printf("LDL : %d, %d, %c \n", node->Length, node->Distance, node->Literal);
+        LzNode* next = node->Next;
+        free(node);
+        node = next;
+    }
+
+    return 1;
+}
 
 int main(void)
 {
-
     char* str = "abcdabcdabcc";
-    EncodeLZ77(str, strlen(str));
-
-    str = "abcdabcdabc"; // failed at this point
-    EncodeLZ77(str, strlen(str));
-
-    str = "abcdefaababc"; // failed at this point
-    EncodeLZ77(str, strlen(str));
+    print_test(str, sizeof(char), strlen(str));
+    str = "abcdabcdabc";
+    print_test(str, sizeof(char), strlen(str));
+    str = "abcdefaababc";
+    print_test(str, sizeof(char), strlen(str));
 
     return 0;
 }
