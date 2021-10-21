7ee5f59f8c5ccc6580bdfebefb0f0a4902ca3beb
Jaejun Jang <jjj404001@gmail.com>
2021-10-20 17:55:11 -0700

    Reimplementing again.

--- lz77.h
+++ lz77.h
@@ -6,6 +6,8 @@ Implementation of lz77 encoding algorith.
 #include <string.h>
 #include <stdio.h>
 
+#define LZMIN(a, b) (((a) < (b)) ? (a) : (b))
+
 typedef struct _lznode
 {
     uint64_t Length;
@@ -33,188 +35,76 @@ LzLinkedlist EncodeLZ77(const void* _src, const size_t _size)
     //const size_t view_count   = 0x200;
     const size_t window_count = 0x06;
     const size_t view_count   = 0x04;
-    const size_t buffer_count = window_count + view_count;
+    const size_t extra_count  = LZMIN(window_count, view_count);
+    const size_t buffer_count = window_count + view_count + (extra_count * 2);
 
     const size_t window_size = window_count * sizeof(uint8_t);
     const size_t view_size   = view_count   * sizeof(uint8_t);
-    const size_t buffer_size = window_size + view_size;
-    
-    uint8_t* buffer = (uint8_t*)malloc(window_size + view_size);
-    uint8_t* window = &buffer[0];
-    uint8_t* view   = &buffer[window_count]; 
+    const size_t buffer_size = buffer_count * sizeof(uint8_t);
 
+    
+    uint8_t* buffer = (uint8_t*)malloc(buffer_size);
+    uint8_t* window = &buffer[0];
+    uint8_t* view   = &buffer[0]; 
+    uint64_t p = 0;
     uint64_t src_index    = 0;
     
     uint64_t total_size = 0;
 
 
-    
     linked_list.Head = (LzNode*)malloc(sizeof(LzNode));
 
     LzNode* current = linked_list.Head;
 
-    uint64_t window_start = window_count -1;
+    uint64_t view_start = 0;
+    uint64_t view_end   = view_count - 1;
+    uint64_t window_start = 0;
     uint64_t window_end   = 0;
 
-    // Fill view buffer with inputs.
-    {
-        size_t curr_view_count = view_count;
-        // First LDD is always be 0,0,view[0]
-        current->Length   = 0;
-        current->Distance = 0;
-        current->Literal  = byte_src[0];
+    memcpy(view, byte_src, view_size);
 
-        ++total_size;
-
-        uint64_t curr_count = 1;
-
-        memcpy(view, &byte_src[1], view_size); // start from 1st data, since 0st data is gonna stored in window[0]
-        window[window_start] = current->Literal;
-        
-        // first iteration to fill up the window buffer
-        while(curr_count < window_count && curr_count < _size)
-        {
-            uint64_t i = window_start;
-
-            while(i < window_count)
-            {
-                if(window[i] == view[0])
-                    break;
-                i++;
-            }
-
-            if (i == window_count) // if i is same with current window size, no p found inside window buffer.
-            {
-                LzNode* new = (LzNode*)malloc(sizeof(LzNode));
-                new->Length = 0;
-                new->Distance = 0;
-                new->Literal  = view[0];
-
-                current->Next = new;
-                current = new;
-            }
-            else
-            {
-                LzNode* new = (LzNode*)malloc(sizeof(LzNode));
-                new->Length   = 0;
-                new->Distance = window_count - i;
-
-                uint64_t max_it = curr_count;
-                if(curr_count > curr_view_count) // TODO: use MIN here.
-                    max_it = curr_view_count;
-
-                uint64_t j = 0;
-                while(max_it && window[i++] == view[j++])
-                {
-                    --max_it;
-                    ++new->Length;
-                }
-                    
-
-                if(!max_it)
-                {
-                    if(new->Length >= view_count)
-                    {
-                        new->Length = view_count-1;
-                        new->Literal = view[new->Length];
-                    }
-                    else // else if literal still sit inside of the buffer
-                    {
-                        new->Literal = view[new->Length];
-                    }
-                }
-                else
-                    new->Literal = view[new->Length];
-
-                current->Next = new;
-                current = new;
-            }
-
-
-            
-            
-            if(window_start >= current->Length +1)
-            {
-                window_start -= current->Length +1;
-                curr_count += current->Length +1;
-                total_size += current->Length +1;
-            }
-            else
-            {
-                curr_count += window_start;
-                total_size += window_start;
-                window_start = 0;
-            }
-                
-            
-            memcpy(&window[window_start], &byte_src[src_index], window_count - window_start + curr_view_count);
-        }
-
-        total_size = curr_count;
-    }
-
-    uint64_t min_count = view_count;
-    if(min_count > window_count) 
-        min_count = window_count;
-
-     
     while(total_size < _size)
     {
         uint64_t i = window_start;
-        while(i < window_count)
+        uint64_t j = view_start;
+        uint64_t inc = 0;
+        while(i < window_end)
         {
-            if(window[i] == view[0])
+            if(window[i] == view[j])
                 break;
-            i++;
+            ++i;
         }
-
-        if (i == window_count) // if i is same with current window size, no p found inside window buffer.
-        {
-            LzNode* new = (LzNode*)malloc(sizeof(LzNode));
-            new->Length = 0;
-            new->Distance = 0;
-            new->Literal  = view[0];
-
-            current->Next = new;
-            current = new;
-        }
-        else
+        // Not found
+        if(i == window_end)
         {
             LzNode* new = (LzNode*)malloc(sizeof(LzNode));
             new->Length   = 0;
-            new->Distance = window_count - i;
+            new->Distance = 0;
+            new->Literal  = view[view_start];
 
-            uint64_t max_it = min_count;
-
-            uint64_t j = 0;
-            while(max_it-- && window[i++] == view[j++])
-                ++new->Length;
-
-            // If src_index goes out of the src.
-            if(total_size + new->Length + 1>= _size) // +1 for literal.
-            {
-                uint64_t diff = _size - total_size;
-                new->Length  = diff - 1;
-                new->Literal = view[new->Length];
-
-                current->Next = new;
-                current = new; 
-                break; // break here to reduce memcpy
-            }
-            else
-                new->Literal = view[new->Length];
-
-            current->Next = new;
+            current->Next = new; 
             current = new;
+
+            inc += 1;
         }
-        total_size += current->Length +1;
-        src_index  += current->Length +1;
-        memcpy(buffer, &byte_src[src_index], buffer_size);
+        
+
+        total_size += inc;
+
+        //window_start += inc;
+        window_end += inc;
+
+        view_start += inc;
+        view_end += inc;
+
+        p += inc;
     }
+
+
+    // pull single slot
+    linked_list.Head = linked_list.Head->Next;
     current->Next = NULL;
-
     free((void*)buffer);
-
     linked_list.SizeInByte = _size;
 
     return linked_list;
--- main.c
+++ main.c
@@ -86,7 +86,7 @@ int compare_and_free_test(void* _buff, size_t _type_size, size_t _buff_count)
 int main(void)
 {
     char* str = "abcdabcdabcc";
-    //compare_and_free_test(str, sizeof(char), strlen(str));
+    print_and_free_test(str, sizeof(char), strlen(str));
     str = "abcdabcdabc";
     print_and_free_test(str, sizeof(char), strlen(str));
     str = "abcdefaababc";
