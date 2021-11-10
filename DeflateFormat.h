2e5f5fc11237cf41cc68df509c60ce307e27ca74
Jaejun Jang <jjj404001@gmail.com>
2021-11-10 15:23:10 -0800

    Added deflate out stream.

--- DeflateFormat.h
+++ DeflateFormat.h
@@ -1,6 +1,8 @@
 #ifndef _DEFLATE_FORMAT_H
 #define _DEFLATE_FORMAT_H
 
+#include <stdint.h>
+
 #define BTYPE_NOCOMP 0b00
 #define BTYPE_FIXED 0b01
 #define BTYPE_DYNAMIC 0b10
@@ -11,5 +13,13 @@
 
 #define FIXED_END_OF_DATA 0b0000000
 #define COMP_END_OF_DATA 0b100000000 // 256
+
+typedef struct _deflate_out_stream
+{
+    uint8_t BitMask;
+    uint8_t BitUsed;
+    uint8_t Data;
+    uint8_t BlockStarted;
+} DeflateOStream;
 
 #endif
