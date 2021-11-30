087f6c5c449fff7f28ac4e0553b91eaea7b5a5a9
Jaejun Jang <jjj404001@gmail.com>
2021-11-30 14:47:02 -0800

    Added memset0.

--- lz77.c
+++ lz77.c
@@ -36,6 +36,7 @@ LzLinkedlist EncodeLZ77(const void* _src, const size_t _size)
     linked_list.NodeCount  = 0;
     linked_list.Head = (LzNode*)malloc(sizeof(LzNode));
     linked_list.DCounter = (uint16_t*)malloc(sizeof(uint16_t) * MAX_D);
+    memset(linked_list.DCounter, 0, sizeof(uint16_t) * MAX_D);
     linked_list.LCounter = (uint16_t**)malloc(sizeof(uint16_t*) * 256);
     
     {
@@ -43,6 +44,7 @@ LzLinkedlist EncodeLZ77(const void* _src, const size_t _size)
         for(i = 0; i < 256; i++)
         {
             linked_list.LCounter[i] = (uint16_t*)malloc(sizeof(uint16_t) * MAX_D);
+            memset(&linked_list.LCounter[i], 0, sizeof(uint16_t) * MAX_D);
         }
     }
     // TODO : MEMSET
